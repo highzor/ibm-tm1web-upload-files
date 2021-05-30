1c04c2599c20e39bbaaae43ea6ab67540d875972
Alexander Petukhov <bnmmnb1337@gmail.com>
2021-05-30 05:23:21 +0300

    feat: base version

--- replaceCommentScript.js
+++ replaceCommentScript.js
@@ -49,9 +49,18 @@ function removeFile(cellElement) {
   };
 
   $.ajax(settings).done(function (response) {
-    const removedFileObject = JSON.parse(response);
+    const removedFileInfoObject = JSON.parse(response);
+    replaceCell(removedFileInfoObject);
     alert('Файл удален!');
   });
+}
+
+function replaceCell(fileInfo) {
+  $(`[data-filename-log="${fileInfo.name}"]`)
+  .html(`<br>
+             <span style="cursor:pointer; border-bottom:1px dashed gray;" 
+               title="Файл '${fileInfo.name}' удален пользователем '${fileInfo.user}' в '${fileInfo.datetime}'">Файл удален
+               </span> - `);
 }
 
 function getUserName() {
@@ -63,5 +72,10 @@ function getFormname() {
   const formname = $('.dijitTabInner.dijitTabContent.dijitClosable.dijitTab.dijitTabChecked.dijitChecked').find($('.dijitTabLabel')).attr("title").split(': ').pop();
   return formname;
 }
+
+// const div = document.createElement('div');
+// div.innerHTML = "<span class=\"dijit dijitReset dijitInline tm1webButton tm1webBtnSecondary dijitButton\" role=\"presentation\" widgetid=\"dijit_form_Button_14\"><span class=\"dijitReset dijitInline dijitButtonNode\" data-dojo-attach-event=\"ondijitclick:__onClick\" role=\"presentation\"><span class=\"dijitReset dijitStretch dijitButtonContents\" data-dojo-attach-point=\"titleNode,focusNode\" role=\"button\" aria-labelledby=\"dijit_form_Button_14_label\" tabindex=\"0\" id=\"dijit_form_Button_14\" style=\"user-select: none;\"><span class=\"dijitReset dijitInline dijitIcon dijitNoIcon\" data-dojo-attach-point=\"iconNode\"></span><span class=\"dijitReset dijitToggleButtonIconChar\">●</span><span class=\"dijitReset dijitInline dijitButtonText\" id=\"dijit_form_Button_14_label\" data-dojo-attach-point=\"containerNode\">Отмена</span></span></span><input type=\"button\" value=\"\" class=\"dijitOffScreen\" data-dojo-attach-event=\"onclick:_onClick\" tabindex=\"-1\" role=\"presentation\" aria-hidden=\"true\" data-dojo-attach-point=\"valueNode\"></span>";
+// let take = document.getElementsByClassName('tm1webDialogButtons')[0];
+// take.appendChild(div.firstElementChild);
 
 export { replaceComments, removeFile }
